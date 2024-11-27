128dbafb97b178fd793c3cbc24f19e0624cb2ac2
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-11-27 15:21:20 +0900

    Feat: TopBar 생성

--- feature/main/src/main/java/kr/co/main/MainScreen.kt
+++ feature/main/src/main/java/kr/co/main/MainScreen.kt
@@ -1,15 +1,32 @@
 package kr.co.main
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.main.navigation.MainNavHost
 import kr.co.main.navigation.MainNavigator
 import kr.co.main.navigation.MainTab
 import kr.co.main.navigation.rememberMainNavigator
+import kr.co.ui.icon.SeeDocsIcon
+import kr.co.ui.icon.seedocsicon.Search
+import kr.co.ui.icon.seedocsicon.Settings
+import kr.co.ui.theme.SeeDocsTheme
 import kr.co.ui.theme.Theme
 
 @Composable
@@ -26,6 +43,9 @@ private fun MainScreen(
     navigator: MainNavigator,
 ) {
     Scaffold(
+        topBar = {
+            MainTopBar()
+        },
         bottomBar = {
             MainBottomBar(
                 visible = navigator.bottomBarVisible(),
@@ -48,5 +68,53 @@ private fun MainScreen(
                 )
             }
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MainTopBar(
+    onSearchClick: () -> Unit = {},
+    onSettingClick: () -> Unit = {},
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = buildAnnotatedString {
+                    append("See")
+                    withStyle(Theme.typography.title1sb.copy(color = Theme.colors.highlight).toSpanStyle()) {
+                        append("Docs")
+                    }
+                },
+                style = Theme.typography.title1sb,
+            )
+        },
+        actions = {
+            Row(
+                modifier = Modifier.padding(end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = SeeDocsIcon.Search,
+                    contentDescription = "파일 검색",
+                )
+
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = SeeDocsIcon.Settings,
+                    contentDescription = "설정",
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    SeeDocsTheme {
+        MainTopBar()
     }
 }
