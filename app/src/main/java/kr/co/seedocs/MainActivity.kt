2770ad4b9916f25a7493f6097148042216840a11
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-12-03 20:48:52 +0900

    Feat: 파일 탐색 권한 요청

--- app/src/main/java/kr/co/seedocs/MainActivity.kt
+++ app/src/main/java/kr/co/seedocs/MainActivity.kt
@@ -1,17 +1,69 @@
 package kr.co.seedocs
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import kr.co.main.Main
 import kr.co.ui.theme.SeeDocsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val requestLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.all { it.value }) {
+                setContent()
+            } else {
+                finish()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        if (hasPermissions()) {
+            setContent()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_MEDIA_VIDEO
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestLauncher.launch(
+                arrayOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                ),
+            )
+        } else {
+            requestLauncher.launch(
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            )
+        }
+    }
+
+    private fun setContent() {
         setContent {
             SeeDocsTheme {
                 Main()
