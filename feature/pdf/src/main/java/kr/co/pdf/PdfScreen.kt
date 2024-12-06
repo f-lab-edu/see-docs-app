b9a36cdb72b24babdc95cbca79b58ebc26642ba7
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-12-06 09:37:58 +0900

    Feat: PDF Zoom & drag

--- feature/pdf/src/main/java/kr/co/pdf/PdfScreen.kt
+++ feature/pdf/src/main/java/kr/co/pdf/PdfScreen.kt
@@ -5,21 +5,30 @@ import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.ui.theme.SeeDocsTheme
 import kr.co.ui.theme.Theme
+import net.engawapg.lib.zoomable.rememberZoomState
+import net.engawapg.lib.zoomable.zoomable
 import java.io.File
 
 @Composable
@@ -55,6 +64,8 @@ private fun PdfScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Theme.colors.bg)
+            .zoomable(rememberZoomState()),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         items(renderer.pageCount) { page ->
             PdfImage(
@@ -73,22 +84,46 @@ private fun PdfImage(
     var bitmap by remember {
         mutableStateOf<Bitmap?>(null)
     }
+    var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(Unit) {
+        isLoading = true
         val page = renderer.openPage(pageIndex)
         Bitmap.createBitmap(page.width, page.height, Bitmap.Config.ARGB_8888).also {
-            page.render(it, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            page.render(it, null, null, PdfRenderer.Page.RENDER_MODE_FOR_PRINT)
             bitmap = it
             page.close()
+            isLoading = false
         }
     }
 
-    bitmap?.let {
-        Image(
-            bitmap = it.asImageBitmap(),
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
-        )
+    if (isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = ColorPainter(Theme.colors.grayText),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            CircularProgressIndicator(
+                color = Theme.colors.highlight
+            )
+        }
+    } else {
+        bitmap?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(it.width.toFloat() / it.height.toFloat())
+            )
+        }
     }
 }
 
