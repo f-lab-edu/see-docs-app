0411c4ed76a1ed8f898012df2881fd8b2c68d4bd
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-12-07 19:49:43 +0900

    Feat: PDF TopBar Page calculation

--- core/ui/src/main/java/kr/co/ui/widget/TextField.kt
+++ core/ui/src/main/java/kr/co/ui/widget/TextField.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -15,28 +15,43 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.ui.theme.SeeDocsTheme
 import kr.co.ui.theme.Theme
 
+enum class TextFieldInputType {
+    NUMBER,
+    TEXT
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SimpleTextField(
     value: String,
     onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
     hint: String? = null,
-    modifier: Modifier = Modifier
+    inputType: TextFieldInputType = TextFieldInputType.TEXT,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
+
+    val keyboardOptions = when(inputType) {
+        TextFieldInputType.NUMBER -> KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number
+        )
+        TextFieldInputType.TEXT -> KeyboardOptions.Default
+    }
 
     BasicTextField(
         modifier = modifier,
         value = value,
         onValueChange = onValueChange,
         textStyle = Theme.typography.body1r.copy(color = Theme.colors.text),
-        singleLine = true
+        singleLine = true,
+        keyboardOptions = keyboardOptions,
     ) {
         Box(
             modifier = Modifier
