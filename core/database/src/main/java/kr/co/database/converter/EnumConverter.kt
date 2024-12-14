b71fc3f3d8cdedfda274d1a8a7d1c642ec9c2ff8
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-12-14 15:51:01 +0900

    Fix: RoomDataBase 타입 컨버터 설정

--- core/database/src/main/java/kr/co/database/converter/EnumConverter.kt
+++ core/database/src/main/java/kr/co/database/converter/EnumConverter.kt
@@ -1,10 +1,8 @@
 package kr.co.database.converter
 
-import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import kr.co.database.model.RecentFile
 
-@ProvidedTypeConverter
 internal class EnumConverter {
 
     @TypeConverter
