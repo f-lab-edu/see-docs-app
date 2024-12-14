f1262a1f804cb211d3a0ebbe2ce1d0b19d6e438a
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-12-14 15:52:30 +0900

    Feat: 최근 본 파일 Room 연결

--- feature/recent/src/main/java/kr/co/recent/RecentViewModel.kt
+++ feature/recent/src/main/java/kr/co/recent/RecentViewModel.kt
@@ -1,7 +1,32 @@
 package kr.co.recent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kr.co.data.repository.RecentRepository
+import kr.co.model.RecentUiState
 
-internal class RecentViewModel : ViewModel() {
+internal class RecentViewModel(
+    recentRepository : RecentRepository,
+) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(RecentUiState.EMPTY)
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            recentRepository.get()
+                .collectLatest { files ->
+                    _uiState.update {
+                        it.copy(
+                            files = files
+                        )
+                    }
+                }
+        }
+    }
 }
