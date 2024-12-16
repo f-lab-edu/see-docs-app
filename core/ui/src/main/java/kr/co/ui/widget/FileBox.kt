753a0c4b9af417bf6c3b0e8a660d7d48c4ac3638
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-12-16 13:00:10 +0900

    Fix: Semantics contentDescription 그룹화

--- core/ui/src/main/java/kr/co/ui/widget/FileBox.kt
+++ core/ui/src/main/java/kr/co/ui/widget/FileBox.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.ui.icon.SeeDocsIcon
@@ -33,7 +35,10 @@ fun FileBox(
             .padding(
                 vertical = 12.dp,
                 horizontal = 8.dp
-            ),
+            )
+            .semantics {
+                contentDescription = name
+            },
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(24.dp)
     ) {
@@ -46,14 +51,20 @@ fun FileBox(
         )
 
         Column(
+            modifier = Modifier
+                .clearAndSetSemantics { },
             verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
             Text(
+                modifier = Modifier
+                    .clearAndSetSemantics { },
                 text = name,
                 style = Theme.typography.body2sb,
                 color = Theme.colors.text
             )
             Text(
+                modifier = Modifier
+                    .clearAndSetSemantics { },
                 text = "2023.01.01",
                 style = Theme.typography.caption1r,
                 color = Theme.colors.grayText
