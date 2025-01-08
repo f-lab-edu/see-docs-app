5b2cd293dbfc6d295c0394609f1285c246114d23
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-09 01:59:26 +0900

    Chore: 파라미터명 p1 -> from

--- core/data/src/main/java/kr/co/data/mapper/RecentFileMapper.kt
+++ core/data/src/main/java/kr/co/data/mapper/RecentFileMapper.kt
@@ -5,18 +5,18 @@ import kr.co.database.model.RecentFile
 import kr.co.model.FileInfo
 
 internal class RecentFileMapper : Mapper<FileInfo, RecentFile> {
-    override fun invoke(p1: FileInfo): RecentFile =
+    override fun invoke(from: FileInfo): RecentFile =
         RecentFile(
-            name = p1.name,
-            path = p1.path,
-            type = when (p1.type) {
+            name = from.name,
+            path = from.path,
+            type = when (from.type) {
                 FileInfo.Type.PDF -> RecentFile.Type.PDF
                 FileInfo.Type.IMAGE -> RecentFile.Type.IMAGE
                 FileInfo.Type.FOLDER -> RecentFile.Type.PDF
             },
-            size = p1.size,
-            createdAt = p1.createdAt,
-            lastModified = p1.lastModified
+            size = from.size,
+            createdAt = from.createdAt,
+            lastModified = from.lastModified
         )
 
     fun toFileInfo(recentFile: RecentFile): FileInfo =
