6ad37ef793bff64ef9f69676939fff1272e0fc0f
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-10 02:52:06 +0900

    Refactor: LaunchIntentHandler 파라미터 수정 Generic -> *

--- core/ui/src/main/java/kr/co/ui/util/UiEffect.kt
+++ core/ui/src/main/java/kr/co/ui/util/UiEffect.kt
@@ -2,16 +2,14 @@ package kr.co.ui.util
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import kotlinx.coroutines.flow.Flow
 import kr.co.ui.base.BaseMviViewModel
 import kr.co.ui.base.UiIntent
 import kr.co.ui.base.UiSideEffect
-import kr.co.ui.base.UiState
 
 @Composable
-fun <S: UiState, I: UiIntent, E: UiSideEffect>LaunchIntentHandler(
+fun <I: UiIntent>LaunchIntentHandler(
     intent: I,
-    viewModel: BaseMviViewModel<S, I, E>,
+    viewModel: BaseMviViewModel<*, I, *>,
     key: Any = Unit,
 ) {
     LaunchedEffect(key, intent) {
@@ -20,26 +18,13 @@ fun <S: UiState, I: UiIntent, E: UiSideEffect>LaunchIntentHandler(
 }
 
 @Composable
-fun <S: UiState, I: UiIntent, E: UiSideEffect>LaunchSideEffect(
-    viewModel: BaseMviViewModel<S, I, E>,
+fun <E: UiSideEffect>LaunchSideEffect(
+    viewModel: BaseMviViewModel<*, *, E>,
     key: Any = Unit,
     onEffect: suspend (E) -> Unit,
 ) {
     LaunchedEffect(key) {
         viewModel.sideEffect.collect {
-            onEffect(it)
-        }
-    }
-}
-
-@Composable
-fun <T> UiEffect(
-    stream: Flow<T>,
-    key: Any = Unit,
-    onEffect: suspend (T) -> Unit,
-) {
-    LaunchedEffect(key) {
-        stream.collect {
             onEffect(it)
         }
     }
