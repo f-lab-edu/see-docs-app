3ffe81c666f587d81206c794f508cbac586efffb
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-11 00:06:48 +0900

    Fix: Make this interface functional or replace it with a function type.

--- feature/explore/src/main/java/kr/co/util/IFileManager.kt
+++ feature/explore/src/main/java/kr/co/util/IFileManager.kt
@@ -2,6 +2,6 @@ package kr.co.util
 
 import kr.co.model.FileInfo
 
-interface IFileManager {
+internal fun interface IFileManager {
     suspend fun readPDFOrDirectory(path: String): List<FileInfo>
 }
