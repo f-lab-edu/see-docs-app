6f85904726ed212828bdc0071b016686c6a62fac
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-14 09:42:41 +0900

    Refactor: Delete Test Repository

--- feature/bookmark/src/test/kotlin/kr/co/bookmark/BookmarkViewModelTest.kt
+++ feature/bookmark/src/test/kotlin/kr/co/bookmark/BookmarkViewModelTest.kt
@@ -1,41 +1,54 @@
 package kr.co.bookmark
 
 import app.cash.turbine.test
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import kr.co.data.repository.BookmarkRepository
+import kr.co.data.repository.RecentRepository
 import kr.co.model.BookmarkSideEffect
 import kr.co.model.BookmarkUiIntent
 import kr.co.model.FileInfo
 import kr.co.model.FileInfo.Type.PDF
-import kr.co.testing.repository.TestBookmarkRepository
-import kr.co.testing.repository.TestRecentRepository
 import kr.co.testing.rule.CoroutineTestRule
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.time.LocalDateTime
+import kotlin.test.assertEquals
 
 internal class BookmarkViewModelTest {
 
     @get: Rule
     val coroutineTestRule = CoroutineTestRule()
 
-    private val bookmarkRepository = TestBookmarkRepository()
-    private val recentRepository = TestRecentRepository()
+    @MockK
+    private lateinit var bookmarkRepository: BookmarkRepository
+
+    @MockK
+    private lateinit var recentRepository: RecentRepository
 
     private lateinit var viewModel: BookmarkViewModel
 
     @Before
     fun setup() {
+        MockKAnnotations.init(this)
         viewModel = BookmarkViewModel(bookmarkRepository,recentRepository)
     }
 
     @Test
     fun `Given a Unit when Init intent is handled then state is updated`() = runTest {
+        val dummy = listOf(PDF_DUMMY, PDF_DUMMY)
+
+        coEvery { bookmarkRepository.get() } returns flowOf(dummy)
+
         viewModel.handleIntent(BookmarkUiIntent.Init)
 
         viewModel.uiState.test {
             val state = awaitItem()
-            assert(state.files.isEmpty())
+            assertEquals(state.files, dummy)
         }
     }
 
