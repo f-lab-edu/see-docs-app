78882943617750328cc2e7f5a5830fad81fecceb
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-18 10:59:52 +0900

    Feat: RecentViewModelTest

--- feature/recent/src/test/kotlin/kr/co/recent/RecentViewModelTest.kt
+++ feature/recent/src/test/kotlin/kr/co/recent/RecentViewModelTest.kt
@@ -1,68 +1,61 @@
-package kr.co.bookmark
+package kr.co.recent
 
-import app.cash.turbine.test
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import kr.co.data.repository.BookmarkRepository
 import kr.co.data.repository.RecentRepository
-import kr.co.model.BookmarkSideEffect
-import kr.co.model.BookmarkUiIntent
 import kr.co.model.FileInfo
 import kr.co.model.FileInfo.Type.PDF
+import kr.co.model.RecentSideEffect
+import kr.co.model.RecentUiIntent
 import kr.co.testing.rule.CoroutineTestRule
+import kr.co.testing.util.testWithItem
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.time.LocalDateTime
 import kotlin.test.assertEquals
 
-internal class BookmarkViewModelTest {
+internal class RecentViewModelTest {
 
     @get: Rule
     val coroutineTestRule = CoroutineTestRule()
 
-    @MockK
-    private lateinit var bookmarkRepository: BookmarkRepository
+    private lateinit var viewModel: RecentViewModel
 
     @MockK
     private lateinit var recentRepository: RecentRepository
 
-    private lateinit var viewModel: BookmarkViewModel
-
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        viewModel = BookmarkViewModel(bookmarkRepository,recentRepository)
+        viewModel = RecentViewModel(recentRepository)
     }
 
     @Test
     fun `Given a Unit when Init intent is handled then state is updated`() = runTest {
         val dummy = listOf(PDF_DUMMY, PDF_DUMMY)
 
-        coEvery { bookmarkRepository.get() } returns flowOf(dummy)
+        coEvery { recentRepository.get() } returns flowOf(dummy)
 
-        viewModel.handleIntent(BookmarkUiIntent.Init)
+        viewModel.handleIntent(RecentUiIntent.Init)
 
-        viewModel.uiState.test {
-            val state = awaitItem()
-            assertEquals(state.files, dummy)
+        viewModel.uiState.testWithItem {
+            assertEquals(files, dummy)
         }
     }
 
     @Test
     fun `Given a file when ClickFile intent is handled then navigate to pdf`() = runTest {
-        val file = PDF_DUMMY
+        coEvery { recentRepository.insert(PDF_DUMMY) } returns Unit
 
-        viewModel.handleIntent(BookmarkUiIntent.ClickFile(file))
+        viewModel.handleIntent(RecentUiIntent.ClickFile(PDF_DUMMY))
 
-        viewModel.sideEffect.test {
-            awaitItem().also {
-                assert(it is BookmarkSideEffect.NavigateToPdf)
-                assert((it as BookmarkSideEffect.NavigateToPdf).path == file.path)
-            }
+        viewModel.sideEffect.testWithItem {
+            assert(this is RecentSideEffect.NavigateToPdf)
+            assertEquals((this as RecentSideEffect.NavigateToPdf).path,PDF_DUMMY.path)
         }
     }
 
