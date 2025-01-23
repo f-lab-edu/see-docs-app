6e554f3c33ef446c5299c0464833e3dcc69c3649
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-24 02:49:25 +0900

    Feat: RoomTestRule 추가, Robolectric 환경에서 테스트

--- core/database/src/test/kotlin/kr/co/database/dao/BookmarkFileDaoTest.kt
+++ core/database/src/test/kotlin/kr/co/database/dao/BookmarkFileDaoTest.kt
@@ -1,10 +1,9 @@
 package kr.co.database.dao
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
+import kr.co.database.RoomTestRule
 import kr.co.database.SeeDocsDatabase
 import kr.co.database.dummy.DataBaseDummy
 import kr.co.database.model.BookmarkFile
@@ -13,36 +12,37 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 import java.io.IOException
 
+@RunWith(RobolectricTestRunner::class)
 internal class BookmarkFileDaoTest {
 
-    private lateinit var database: SeeDocsDatabase
     private lateinit var bookmarkFileDao: BookmarkFileDao
+
+    @get: Rule
+    val roomTestRule = RoomTestRule()
+
+    private val db: SeeDocsDatabase = roomTestRule.db
 
     @get: Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setup() {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        database = Room.inMemoryDatabaseBuilder(
-            context,
-            SeeDocsDatabase::class.java
-        ).allowMainThreadQueries().build()
-
-        bookmarkFileDao = database.bookmarkFileDao()
+        bookmarkFileDao = db.bookmarkFileDao()
     }
 
     @After
     @Throws(IOException::class)
     fun close() {
-        database.close()
+        db.close()
     }
 
     @Test
     @Throws(Exception::class)
-    fun bookmarkInsert() = runTest {
+    fun `Given bookmarkFile When insert Then get bookmarkFile`() = runTest {
         val bookmarkFile = DataBaseDummy.BOOKMARK_DUMMY
 
         bookmarkFileDao.insert(bookmarkFile)
@@ -54,7 +54,7 @@ internal class BookmarkFileDaoTest {
 
     @Test
     @Throws(Exception::class)
-    fun bookmarkDelete() = runTest {
+    fun `Given bookmarkFile When delete Then get emptyList`() = runTest {
         val bookmarkFile = DataBaseDummy.BOOKMARK_DUMMY
 
         bookmarkFileDao.insert(bookmarkFile)
